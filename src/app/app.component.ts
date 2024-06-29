c632b2be476c043b4c2e1d1a10a41db2507f382b
bhautik-jivani <jivanibhautik@gmail.com>
2024-06-29 21:00:52 +0530

    Complete Investment Calculator based on Signal and Added additional Drop down to see output based on currency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+
+import type { InvestmentInput } from './investment-input.model';
 
 @Component({
     selector: 'app-root',
@@ -9,4 +12,55 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
     templateUrl: './app.component.html',
     imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent]
 })
-export class AppComponent {}
+export class AppComponent {
+  // resultData?: {
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[]
+  // currency!: string
+
+  currency = signal<string>('')
+  resultData = signal<{
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[] | undefined >(undefined)
+
+  onChangeCurrencyCode(currencyCode: string) {
+    // this.currency = currencyCode
+    this.currency.set(currencyCode)
+  }
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration  } = data
+
+    const annualData = []
+    let investmentValue = initialInvestment
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100)
+      investmentValue += interestEarnedInYear + annualInvestment
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year
+      })
+    }
+
+    // this.resultData = annualData
+    this.resultData.set(annualData)
+
+  }
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import type { InvestmentInput } from '../investment-input.model';
+import { CURRENCIES } from '../currencies';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  // @Output() calculate = new EventEmitter<InvestmentInput>()
+  // @Output() updatedCurrencyCode = new EventEmitter<string>()
+  calculate = output<InvestmentInput>()
+  updatedCurrencyCode = output<string>()
 
+  enteredInitialInvestment = signal('0')
+  enteredAnnualInvestment = signal('0')
+  enteredExpectedReturn = signal('5')
+  enteredDuration = signal('10')
+  enteredCurrency = signal('USD')
+
+  currencyObject: {[key: string]: string} = CURRENCIES
+  currencyKeys: string[] = Object.keys(this.currencyObject)
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    })
+    this.updatedCurrencyCode.emit(this.enteredCurrency())
+
+    this.enteredInitialInvestment.set('0')
+    this.enteredAnnualInvestment.set('0')
+    this.enteredExpectedReturn.set('5')
+    this.enteredDuration.set('10')
+    this.enteredCurrency.set('USD')
+  }
 }
